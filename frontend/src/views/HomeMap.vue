b14041d904658b7692ce49f84a72af8bf20a0c15
M.Goto <sindicum@icloud.com>
2021-06-01 00:24:46 +0900

    modify circle drow bug

--- frontend/src/views/HomeMap.vue
+++ frontend/src/views/HomeMap.vue
@@ -8,36 +8,172 @@
 </template>
 
 <script>
-import MapDrow from '../mixins/MapDrow'
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+delete  L.Icon.Default.prototype._getIconUrl
 
 export default {
   data(){
     return {
+      mapid: "mainmap",
       apiError: false
     }
   },
   mounted() {
-  // プロパティ初期値定義
+    // プロパティ初期値の定義
     var centerLatlng = [43.2121696, 143.2725181]
     var zoomLv = 15
     var mapInfo = true
 
-  // MapDrowのbaseMapDrowメソッド
-    this.baseMapDrow(centerLatlng,zoomLv,mapInfo)
+    // OpenStreetMapの定義
+    const o_std = new L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        });
+    // 地理院タイルの定義
+    const t_ort = new L.tileLayer("http://cyberjapandata.gsi.go.jp/xyz/ort/{z}/{x}/{y}.jpg", {
+        attribution: "<a href='http://www.gsi.go.jp/kikakuchousei/kikakuchousei40182.html' target='_blank'>国土地理院</a>"
+        });
 
-  // axiosでbackendからポリゴンデータを取得
-  // response.dataをMapDrowの各メソッドに渡す
-    // this.mymap -> MapDrowのmymap
-    // this.axios
-    //     .get("/api/v1/polygondata/")
-    //     .then( response => ( this.polygonControl(response.data,this.mymap)) )
-    //     .catch( error => this.apiError = error)
+    // leafletアイコンの設定
+    L.Icon.Default.mergeOptions({
+      iconUrl: require( 'leaflet/dist/images/marker-icon.png'),
+      iconRetinaUrl: require( 'leaflet/dist/images/marker-icon-2x.png'),
+      shadowUrl: require( 'leaflet/dist/images/marker-shadow.png' )
+    })
+
+    // 地図表示
+    let mymap = L.map( this.mapid , { center: L.latLng(centerLatlng), zoom: zoomLv,layers: [o_std] } )
+
+    // 地図補足表示
+    if (mapInfo === true) {
+      const baseMaps = {'OSM': o_std,'地理院': t_ort};
+      L.control.layers(baseMaps, null,{collapsed: false}).addTo(mymap);
+      L.control.scale({imperial: false,maxWidth: 300}).addTo(mymap);
+    }
+
+
+  // axiosでbackendからポリゴンデータを取得、response.dataをMapDrowの各メソッドに渡す
+    this.axios
+        .get("/api/v1/polygondata/")
+        .then( response => ( this.polygonControl(response.data,mymap)) )
+        .catch( error => this.apiError = error)
     this.axios
         .get("/api/v1/soildata/")
-        .then( response => ( this.pointControl(response.data,this.mymap)) )
+        .then( response => ( this.pointControl(response.data,mymap)) )
         .catch( error => this.apiError = error)
+
   },
-  mixins: [MapDrow]
+  methods: {
+    pointControl: function(pointjson,mymap) {
+
+      // pointジオメトリの描画
+      let pointJSON = L.geoJSON(pointjson,{ onEachFeature: onEachFeature, pointToLayer: customCircleMarker }).addTo(mymap)
+
+      // マウスイベント制御
+      function onEachFeature(feature, layer) {
+        layer.on({
+            mouseover: highlightFeature,
+            mouseout: resetHighlight,
+        });
+      }
+
+      // マウスオーバー
+      function highlightFeature(e) {
+          var layer = e.target;
+          layer.setStyle({
+            radius: 40,
+            fillColor: "#ff0000",
+            color: "#ff0000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+          });
+          if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+              layer.bringToFront();
+          }
+      }
+
+      // マウスアウト
+      function resetHighlight(e) {
+          pointJSON.resetStyle(e.target);
+      }
+
+      // サークルアイコンの作成
+      function customCircleMarker (feature, latlng) {
+
+        let potassiumValue = feature['properties']["potassium"]
+
+        var geojsonMarkerOptions = {
+              radius: 40,
+              color: "#000",
+              weight: 1,
+              opacity: 1,
+              fillOpacity: 0.8
+            }
+            
+        if        ( potassiumValue < 8 )  { geojsonMarkerOptions['fillColor'] = "#56C1FF"
+        } else if ( potassiumValue < 15 ) { geojsonMarkerOptions['fillColor'] = "#73FDEA"
+        } else if ( potassiumValue < 30 ) { geojsonMarkerOptions['fillColor'] = "#FFFC66"
+        } else if ( potassiumValue < 50)  { geojsonMarkerOptions['fillColor'] = "#FF9300"
+        } else                            { geojsonMarkerOptions['fillColor'] = "#FF644E"
+        }
+
+        return L.circleMarker(latlng, geojsonMarkerOptions)
+      }
+    },
+    polygonControl: function(polyjson,mymap) {
+
+      // polygonジオメトリの描画
+      let polyJSON = L.geoJSON(polyjson,{ onEachFeature: onEachFeature}).addTo(mymap)
+
+      // マウスイベント制御
+      function onEachFeature(feature, layer) {
+        layer.on({
+            mouseover: highlightFeature,
+            mouseout: resetHighlight,
+            click: moveGeoInfo
+        });
+      }
+
+      // マウスオーバー:ポリゴン情報の表示
+      function highlightFeature(e) {
+          var layer = e.target;
+          layer.setStyle({
+              weight: 5,
+              color: '#ff0000',
+              dashArray: '',
+              fillOpacity: 0.7
+          });
+          if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+              layer.bringToFront();
+          }
+          info.update(layer.feature.properties);
+      }
+
+      // マウスアウト:ポリゴン情報の非表示
+      function resetHighlight(e) {
+          polyJSON.resetStyle(e.target);
+          info.update();
+      }
+
+      // 凡例の表示
+      var info = L.control();
+      info.onAdd = function () {
+          this._div = L.DomUtil.create('div', 'info');
+          this.update();
+          return this._div;
+      };
+      info.update = function (props) {
+          this._div.innerHTML = (props ? '<b>' + props.year + '</b><br />' + props.remarks : 'Field Info');
+      };
+      info.addTo(mymap);
+
+      // ポリゴンクリックで遷移
+      function moveGeoInfo() {
+        window.location.href ="http://127.0.0.1:8000/fieldinfo"
+      }
+    },
+  },
 }
 </script>
 
